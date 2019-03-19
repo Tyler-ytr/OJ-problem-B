3e111c36cf49522c3685263cece7602844f01780
larry <601576661@qq.com>
2019-03-19 21:14:51 +0800

    version 1

---
+++ descriptions/hint.md
@@ -0,0 +1,28 @@
+﻿目前的板子因为只能容纳一个sample,我担心不清楚因此在HINT里面写另外的3个
+
+Input:
+2 1 0
+10 10
+Output:
+20
+
+Input:
+1 2 1
+5 2
+Output:
+2
+
+Input:
+3 2 1
+1 2 3 4 5 6
+Output:
+0
+
+
+In the first example you can form the following barrels: [1, 2], [2, 2], [2, 3], [2, 3].
+
+In the second example you can form the following barrels: [10], [10].
+
+In the third example you can form the following barrels: [2, 5].
+
+In the fourth example difference between volumes of barrels in any partition is at least 2 so it is impossible to make barrels equal enough.
---
+++ src/std.cpp
@@ -0,0 +1,51 @@
+﻿#include <bits/stdc++.h>
+
+# define ll long long
+# define ios ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL);
+# define pb push_back
+# define pob pop_back
+# define pf push_front
+# define pof pop_front
+
+using namespace std;
+
+const ll N = 1e5 + 10;
+
+ll n, m, k, a[N], cnt, x, i, ans;
+map <int, int> d;
+
+int main() {
+    ios
+    cin >> n >> k >> m;
+    for(i = 1;i <= n * k;i++){
+        cin >> a[i];
+        d[a[i]]++;
+    }
+    sort(a + 1, a + n * k + 1);
+    if(a[n] - a[1] > m){
+        cout << 0;
+        return 0;
+    }
+    for(i = n;i <= n * k;i++)
+    {
+        if(a[i] - a[1] <= m)
+            x = i;
+        else
+            break;
+    }
+    cnt = x - n;
+    i = 1;
+    while(cnt > 0)
+    {
+        ans += a[i];
+        i += min(cnt,k - 1);
+        i++;
+        cnt -= min(cnt, k - 1);
+    }
+    while(i <= x)
+    {
+        ans += a[i];
+        i++;
+    }
+    cout << ans;
+}
